c8793446b316d5595f3fd0aaa6113f73ff13779b
Björn May <bjornmay@MacBook-Pro-von-Bjorn.fritz.box>
2022-07-01 16:06:17 +0200

    fixes

--- src/Dto/SleeperMatchup.php
+++ src/Dto/SleeperMatchup.php
@@ -5,6 +5,10 @@ namespace HansPeterOrding\SleeperApiClient\Dto;
 
 class SleeperMatchup
 {
+    private array $startersPoints = [];
+
+    private array $playersPoints = [];
+
     private array $starters = [];
 
     private int $rosterId;
@@ -15,5 +19,93 @@ class SleeperMatchup
 
     private float $points;
 
-    private float $customPoints;
+    private ?float $customPoints;
+
+    public function getStartersPoints(): array
+    {
+        return $this->startersPoints;
+    }
+
+    public function setStartersPoints(array $startersPoints): SleeperMatchup
+    {
+        $this->startersPoints = $startersPoints;
+        return $this;
+    }
+
+    public function getPlayersPoints(): array
+    {
+        return $this->playersPoints;
+    }
+
+    public function setPlayersPoints(array $playersPoints): SleeperMatchup
+    {
+        $this->playersPoints = $playersPoints;
+        return $this;
+    }
+
+    public function getStarters(): array
+    {
+        return $this->starters;
+    }
+
+    public function setStarters(array $starters): SleeperMatchup
+    {
+        $this->starters = $starters;
+        return $this;
+    }
+
+    public function getRosterId(): int
+    {
+        return $this->rosterId;
+    }
+
+    public function setRosterId(int $rosterId): SleeperMatchup
+    {
+        $this->rosterId = $rosterId;
+        return $this;
+    }
+
+    public function getPlayers(): array
+    {
+        return $this->players;
+    }
+
+    public function setPlayers(array $players): SleeperMatchup
+    {
+        $this->players = $players;
+        return $this;
+    }
+
+    public function getMatchupId(): int
+    {
+        return $this->matchupId;
+    }
+
+    public function setMatchupId(int $matchupId): SleeperMatchup
+    {
+        $this->matchupId = $matchupId;
+        return $this;
+    }
+
+    public function getPoints(): float
+    {
+        return $this->points;
+    }
+
+    public function setPoints(float $points): SleeperMatchup
+    {
+        $this->points = $points;
+        return $this;
+    }
+
+    public function getCustomPoints(): ?float
+    {
+        return $this->customPoints;
+    }
+
+    public function setCustomPoints(?float $customPoints): SleeperMatchup
+    {
+        $this->customPoints = $customPoints;
+        return $this;
+    }
 }
